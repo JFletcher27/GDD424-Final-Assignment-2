143f4db76f4af4eee1f1e06fb46a06627c9f4e7b
JFletcher27 <jackf2004@hotmail.co.uk>
2026-05-04 11:49:02 +0100

    Door script added and working

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+
+    [SerializeField] Animator doorAnimator;
+    [SerializeField] GameObject player;
+    [SerializeField] Transform playerTransform;
+    [SerializeField] float openingDistance = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorAnimator = GetComponent<Animator>();
+        player = GameObject.FindWithTag("Player");
+        playerTransform = player.transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float dist = Vector3.Distance(playerTransform.position, transform.position);
+
+        if (doorAnimator != null)
+        {
+            if (dist <= openingDistance)
+            {
+                doorAnimator.SetTrigger("PlayerOpenDoor");
+            }
+        }
+    }
+
+    
+}
